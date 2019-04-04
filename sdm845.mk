7c171c0f2866077b44c673b7505ade691944d341
Ramakant Singh <ramakant@codeaurora.org>
2019-04-04 11:03:37 +0530

    sdm845: Upgrade configstore to 1.1
    Upgrade configstore to 1.1 in accordance with upstream changes.
    
    Change-Id: Ifea344a42608039115035c8bdebb13e49e3b9282

--- sdm845.mk
+++ sdm845.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service \
+    android.hardware.configstore@1.1-service \
     android.hardware.broadcastradio@1.0-impl
 
 # Vibrator
