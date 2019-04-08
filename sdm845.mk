ac090d26f29af7a0bebd3f62c0892fda8c8051ae
padarshr <padarshr@codeaurora.org>
2019-04-08 23:03:25 +0530

    Remove brillo_update_payload from target PRODUCT_PACKAGES
    brillo_update_payload is already under PRODUCT_HOST_PACKAGES,
    remove it from target PRODUCT_PACKAGES.
    
    Change-Id: Ifc2d64ab2f85703654865df98fa7d5b3360c5ee9

--- sdm845.mk
+++ sdm845.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += update_engine \
     update_engine_client \
     update_verifier \
     bootctrl.sdm845 \
-    brillo_update_payload \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
