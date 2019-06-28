92733924e209b6739c1cdc37b4932e74341a4558
Pawan Chilka <pchilka@codeaurora.org>
2019-06-28 12:07:13 +0530

    Revert "Revert "sdm845: Set Header version for bootimage".".
    This reverts commit d06226baa96dbae23ed061b245d95fde013183c8.
    
    Change-Id: I22dcbbc38583e9c558c841f169ffead715a635c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,10 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common
 USE_OPENGL_RENDERER := true
 BOARD_USE_LEGACY_UI := true
 
+# Set Header version for bootimage
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
 ifeq ($(ENABLE_AB), true)
 # Defines for enabling A/B builds
 AB_OTA_UPDATER := true
